ad185d2b21aa9bcbd14d59cde32f2f63a6fcb316
zyoun007 <146486881+zyoun007@users.noreply.github.com>
2023-12-01 15:33:05 -0600

    Add files via upload

---
+++ functionLab.py
@@ -0,0 +1,7 @@
+dinnerPlans = ['turkey', 'corn', 'water', 'uhh, already more than i usually eat'] 
+print(dinnerPlans)
+print(len(dinnerPlans))
+
+breakfastPlans = ['between absolutely nothing', 'some dry frosted flakes', 'or some frosted flakes with milk']
+print(breakfastPlans)
+print(len(breakfastPlans))
---
+++ functionLab2.py
@@ -0,0 +1,17 @@
+dinnerPlans = ('Turkey, Corn, Water, Uhh, already more than I usually eat')
+dinnerPlanslabel = ('Plans for my dinner tonight:')
+breakfastPlans = ('between absolutely nothing, some dry frosted flakes, or some frosted flakes with milk')
+breakfastPlanslabel = ('Plans for my breakfast tomorrow:')
+
+
+def print_collection(label, items):
+    print(label, items)
+    print(len(items))
+
+print_collection(dinnerPlanslabel, dinnerPlans)
+
+print_collection(breakfastPlanslabel, breakfastPlans) 
+    
+
+
+    
